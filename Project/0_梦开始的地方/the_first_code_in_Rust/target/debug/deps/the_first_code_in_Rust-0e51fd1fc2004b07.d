91454cf2b5adbeb4f3eee969e46df4dff20e85f2
AL1S9178917896 <1332753871@qq.com>
2026-01-05 15:36:59 +0800

    Delete Project/0_梦开始的地方/the_first_code_in_Rust/target directory

--- Project/0_梦开始的地方/the_first_code_in_Rust/target/debug/deps/the_first_code_in_Rust-0e51fd1fc2004b07.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lenovo\Desktop\rust_codes\Project\the_first_code_in_Rust\target\debug\deps\the_first_code_in_Rust-0e51fd1fc2004b07.d: src\main.rs
-
-C:\Users\Lenovo\Desktop\rust_codes\Project\the_first_code_in_Rust\target\debug\deps\libthe_first_code_in_Rust-0e51fd1fc2004b07.rmeta: src\main.rs
-
-src\main.rs:
