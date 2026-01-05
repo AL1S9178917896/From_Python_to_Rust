9f3860424718b679baf0ccac22820a15d72fc6aa
AL1S9178917896 <1332753871@qq.com>
2026-01-05 15:34:37 +0800

    Add files via upload

---
+++ Project/0_梦开始的地方/the_first_code_in_Rust/target/debug/deps/the_first_code_in_Rust-8670563762d32243.d
@@ -0,0 +1,5 @@
+C:\Users\Lenovo\Desktop\rust_codes\Project\the_first_code_in_Rust\target\debug\deps\the_first_code_in_Rust-8670563762d32243.d: src\main.rs
+
+C:\Users\Lenovo\Desktop\rust_codes\Project\the_first_code_in_Rust\target\debug\deps\libthe_first_code_in_Rust-8670563762d32243.rmeta: src\main.rs
+
+src\main.rs:
